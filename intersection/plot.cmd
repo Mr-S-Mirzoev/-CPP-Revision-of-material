85bc166648287a8ba83766835eeef1e81d9c6d36
Sergey Mirzoev <61053551+Mr-S-Mirzoev@users.noreply.github.com>
2020-06-24 15:19:39 +0500

    Add files via upload

--- intersection/plot.cmd
+++ intersection/plot.cmd
@@ -1,6 +1,7 @@
 set terminal png size 1000,1000
 set output 'result.png'
+set palette model RGB defined ( 1 'violet', 2 'red', 3 'blue', 4 'yellow', 5 'green', 6 'orange', 7 'cyan')
 unset xtics
 unset ytics
 unset border
-plot 'lines.txt' using 1:2:($3-$1):($4-$2) with vectors nohead notitle
+plot 'lines.txt' using 1:2:($3-$1):($4-$2):5 with vectors palette nohead notitle
